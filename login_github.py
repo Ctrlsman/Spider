aab38bd1a88a248f7dde52a4681ada95310bdfbb
zds <673711980@qq.com>
2018-01-11 15:16:45 +0800

    fix login_github

--- login_github.py
+++ login_github.py
@@ -2,17 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 
 result = requests.get("https://github.com/login")
-soup = BeautifulSoup(result.text,'html.parser')
-token = soup.find(name='input',attrs={'name':'authenticity_token'}).get('value')
+soup = BeautifulSoup(result.text, 'html.parser')
+token = soup.find(name='input', attrs={'name': 'authenticity_token'}).get('value')
 r1_cookie_dict = result.cookies.get_dict()
 r2 = requests.post(
     "https://github.com/session",
     data={
-        "utf8":'✓',
-        'authenticity_token':token,
-        'login':'你的账号',
-        'password':'你的密码',
-        'commit':'Sign in'
+        "utf8": '✓',
+        'authenticity_token': token,
+        'login': '673711980@qq.com',
+        'password': 'zds819918',
+        'commit': 'Sign in'
     },
     cookies=r1_cookie_dict
 )
@@ -25,4 +25,4 @@ cookie_dict.update(r2_cookies_dict)
 r3 = requests.get('https://github.com/settings/emails',
                   cookies=cookie_dict)
 print(r3.text)
-#最后检查一下发现登录信息了
+# 最后检查一下发现登录信息了
